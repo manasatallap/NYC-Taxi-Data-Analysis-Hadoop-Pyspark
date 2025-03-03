c9a6b2c846ca77cc0bba545b8e7642bf7f3fcdc9
Manasa Tallapaneni <94345439+manasatallap@users.noreply.github.com>
2025-03-03 11:21:13 -0500

    Update reducer3.py

--- Hadoop/reducer3.py
+++ Hadoop/reducer3.py
@@ -3,10 +3,12 @@ import sys
 
 distinct_ids = set()
 
+# Process each line from standard input
 for line in sys.stdin:
-    line = line.strip()
+    line = line.strip()  # Remove leading/trailing whitespace
     if line:
-        distinct_ids.add(int(line))
+        distinct_ids.add(int(line))  # Add unique drop-off location ID to the set
 
+# Output sorted unique drop-off location IDs
 for DOLocationID in sorted(distinct_ids):
     print(DOLocationID)
