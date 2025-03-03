18703d5b15f8fb6bc2a74d132f00745b1f1e38e5
Manasa Tallapaneni <94345439+manasatallap@users.noreply.github.com>
2025-03-03 11:19:07 -0500

    Update mapper4.py

--- Hadoop/mapper4.py
+++ Hadoop/mapper4.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 import sys
 
+# Process each line from standard input
 for line in sys.stdin:
-    data = line.strip().split(',')
+    data = line.strip().split(',')  # Remove whitespace and split by comma
     try:
-        trip_distance = float(data[4])
-        print(f"{trip_distance}")
+        trip_distance = float(data[4])  # Convert trip distance to float
+        print(f"{trip_distance}")  # Output trip distance
     except (ValueError, IndexError):
-        continue
+        continue  # Skip lines with invalid data
